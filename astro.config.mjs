fd5f153e8944e889af4baedd34cef9231f05730f
ElForaneo-EA <erick2000ayala@gmail.com>
2026-03-06 13:13:35 -0500

    Added config astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+const repository = process.env.GITHUB_REPOSITORY ?? '';
+const [owner = '', repo = ''] = repository.split('/');
+const isUserSite = owner && repo && repo.toLowerCase() === `${owner.toLowerCase()}.github.io`;
+
+const site = owner ? `https://${owner}.github.io` : 'https://example.github.io';
+const base = owner && repo && !isUserSite ? `/${repo}/` : '/';
+
+export default defineConfig({
+	site,
+	base,
+});
